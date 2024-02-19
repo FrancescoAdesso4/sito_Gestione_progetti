1a4629908f8c232a7934e7dbedca80a2c44083b4
FrancescoAdesso4 <adesso.francesco@iisgalvanimi.edu.it>
2024-02-19 09:49:48 +0000

    esempio pug finito

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -3,11 +3,17 @@ const people = require('./people.json'); //Copia il file people.json dentro la v
 const app = express();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+res.render('index', {
+title: 'Homepage',
+people: people.profiles //Passa il vettore profiles alla pagina index.pug
+});
 app.get('/profile', (req, res) => {
     const person = people.profiles.find((p) => p.id === req.query.id);
     res.render('profile', {
-        title: `About ${person.firstname} ${person.lastname}`,
-        person,
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+    });
     });
 });
 app.listen(3000, function () {
