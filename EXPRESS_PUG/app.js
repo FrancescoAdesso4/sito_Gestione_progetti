a00c2bdb0682a7d5f4c2272d73167d31e8490763
FrancescoAdesso4 <adesso.francesco@iisgalvanimi.edu.it>
2024-02-26 20:51:38 +0000

    lavoro finito

--- EXPRESS_PUG/app.js
+++ EXPRESS_PUG/app.js
@@ -1,37 +1,48 @@
 const express = require('express');
-const Asport = require('./json/macchine_sportive.json'); //Copia il file people.json dentro la variabile people
-const Aut = require('./json/macchine_utilitarie.json');
+const autoSportive = require('./json/macchine_sportive.json');
+const autoUtilitarie = require('./json/macchine_utilitarie.json');
+const tutteLeAuto = require('./json/tutte_le_auto.json');
 
 const app = express();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/css'));
+
 app.get('/', (req, res) => {
-
     res.render('home', {
-    title: 'Homepage',
-    utilitarie: Aut['Auto SUV'],
-    sportive: Asport['Auto utilitarie']
-
+        title: 'Homepage',
+        utilitarie: autoUtilitarie['Auto utilitarie'],
+        sportive: autoSportive['Auto Sportive']
     });
 });
 
 app.get('/sportive', (req, res) => {
-
-    res.render('sportive', {
+    res.render('macchine', {
         title: 'Macchine Sportive',
-        utilitarie: Aut['Auto SUV'],
-        sportive: Asport['Auto Sportive']
+        auto: autoSportive['Auto Sportive']
     });
 });
 
 app.get('/utilitarie', (req, res) => {
-
-    res.render('utilitarie', {
-        title: 'Macchine Sportive',
-        utilitarie: Aut['Auto SUV'],
-        sportive: Asport['Auto Sportive']
+    res.render('macchine', {
+        title: 'Macchine Utilitarie',
+        auto: autoUtilitarie['Auto utilitarie']
     });
 });
-app.listen(3000, function () {
-console.log('Example app listening on port 3000!');
+
+app.get('/auto/:id', (req, res) => {
+    const autoId = req.params.id;
+    const auto = tutteLeAuto.Auto.find((auto) => auto.id === parseInt(autoId));
+    if (auto) {
+        res.render('dettagli', {
+            title: `About ${auto.nome}`,
+            d: auto,
+            variabile: 'Auto'
+        });
+    } else {
+        res.status(404).send('Auto non trovata');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
 });
